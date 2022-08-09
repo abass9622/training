600164661d2255b3a68e48fe089e9e58764c8666
abass9622 <mohdabass91@gmail.com>
2022-08-09 09:36:01 -0700

    aug8

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-
+const lodash =require('lodash')
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+    //console.log('My batch is', abc.name)
+   // abc.printName()
+   
+
+    let months = ['January', 'February', 'March', 'April', 'May',
+     'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const a=lodash.chunk(months,3)
+console.log(a)
 
     res.send('My second ever api!')
 });
